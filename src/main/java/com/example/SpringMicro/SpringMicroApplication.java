c9d8b9971b02f29b5a2b8375993a06cb067c08ac
Thadeu Costa <Thadeu.Costa@vistajet.com>
2018-11-08 18:23:43 -0200

    Internationalization and xml support

--- src/main/java/com/example/SpringMicro/SpringMicroApplication.java
+++ src/main/java/com/example/SpringMicro/SpringMicroApplication.java
@@ -2,11 +2,25 @@ package com.example.SpringMicro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringMicroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
 	}
 }
