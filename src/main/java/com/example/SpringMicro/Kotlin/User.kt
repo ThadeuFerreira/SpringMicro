d233a38c511558d6ed86f89371a4ba4649caf8d7
Thadeu Costa <Thadeu.Costa@vistajet.com>
2018-11-12 16:30:35 -0200

    Data base begins

--- src/main/java/com/example/SpringMicro/Kotlin/User.kt
+++ src/main/java/com/example/SpringMicro/Kotlin/User.kt
@@ -1,10 +1,24 @@
 package com.example.SpringMicro.Kotlin
 
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 import java.util.Date
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.validation.constraints.Past
+import javax.validation.constraints.Size
 
+@ApiModel(description="All detaisl about the user.")
+@Entity
 open class User {
+    @Id
+    @GeneratedValue
     var id: Int? = null
+    @Size(min=2, message="Name should have at least 2 cahracters")
     var name: String? = null
+    @Past
+    @ApiModelProperty(notes="Birht date should be in the past")
     var birthDay: Date? = null
 
     constructor() {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values(1, sysdate(), 'Thadeu')
+insert into user values(2, sysdate(), 'Andre')
+insert into user values(3, sysdate(), 'Thiago')
+insert into user values(4, sysdate(), 'Henrique')
